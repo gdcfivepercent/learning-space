24a9a2f469e60f28ac128f44d12334cf52a98a88
Success Gao <13791720+gdcfivepercent@users.noreply.github.com>
2019-04-07 22:44:42 +0800

    play with menu

---
+++ c/terminals/menu.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *choices[] = {
+    "a - Add new record",
+    "d - Display records",
+    "q - Quit program",
+    NULL
+};
+
+int getchoice(char *greet, char *choices[]);
+
+int main(void)
+{
+    int choice;
+
+    do {
+        choice = getchoice("Choose one", choices);
+        printf("You have chosen: %c\n", choice);
+    } while (choice != 'q');
+    exit(EXIT_SUCCESS);
+}
+
+int getchoice(char *greet, char *choices[])
+{
+    int chosen = 0;
+    int selected;
+    char **option;
+
+    do {
+        printf("Menu: %s\n", greet);
+        option = choices;
+        while (*option) {
+            printf("%s\n", *option);
+            option++;
+        }
+        selected = getchar();
+        option = choices;
+        while (*option) {
+            if (selected == *option[0]) {
+                chosen = 1;
+                break;
+            }
+            option++;
+        }
+        if (!chosen) {
+            printf("Incorrect choice, select again!\n");
+        }
+    } while (!chosen);
+
+    return selected;
+}
