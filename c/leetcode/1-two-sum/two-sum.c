cfe850e4ff222096b7ceb38bdf2437ea1047fad1
Success Gao <13791720+gdcfivepercent@users.noreply.github.com>
2019-04-21 17:58:45 +0800

    fix cs

--- c/leetcode/1-two-sum/two-sum.c
+++ c/leetcode/1-two-sum/two-sum.c
@@ -27,7 +27,7 @@ twoSum(int *nums, int numsSize, int target)
     for (i = 0; i < numsSize; i++) {
         for (j = i+1; j < numsSize; j++) {
             if (*(nums+i) + *(nums+j) == target) {
-goto END;
+                goto END;
             }
         }
     }
